1bd90b28ab68bce46ca110ea504dd88a18878b1d
shraja7 <shraja7@gmail.com>
2023-01-22 15:24:25 -0600

    finish ex.2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
-};
+    let result = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    }
+for(let i=0; i<num; i++){
+//repeat the string num times
+
+result+=str;
+}
+return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
