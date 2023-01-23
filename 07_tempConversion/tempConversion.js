d84ae3cb516ef46ac691b480844d3828da551df3
shraja7 <shraja7@gmail.com>
2023-01-22 21:03:09 -0600

    complete ex.7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+let c = (f - 32) * 5/9;
+
+
+return parseFloat(c.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+let f = c * 9/5 + 32;
+return parseFloat(f.toFixed(1))
 };
 
 // Do not edit below this line
