ce6e06dfd07bab236c183b48277b28034b8ae841
shraja7 <shraja7@gmail.com>
2023-01-22 20:35:07 -0600

    complete ex.6 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,23 @@
-const leapYears = function() {
+const leapYears = function(yr) {
+    //determine if year is a leap year
+    //if year is divisible by 4, it is a leap year
+    //if year is divisible by 100, it is not a leap year
+    //if year is divisible by 400, it is a leap year
+    if(yr % 4 === 0){
+        if(yr % 100 === 0){
+            if(yr % 400 === 0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return true;
+        }
+    }else{
+        return false;
+    }
+
+    
 
 };
 
