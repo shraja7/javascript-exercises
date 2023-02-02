dff7c260712569e314d15d4339f160b7fdf78a8c
shraja7 <shraja7@gmail.com>
2023-02-02 14:34:56 -0600

    complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+//take the string and place it in an array
+//reverse the array
+//compare the two arrays
+//if they are the same return true
+const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+const reversedStr = cleanedStr.split('').reverse().join('');
+return cleanedStr === reversedStr;
 
-};
-
+}
 // Do not edit below this line
 module.exports = palindromes;
